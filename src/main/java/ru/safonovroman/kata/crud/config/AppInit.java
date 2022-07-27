885f1102800bc1240468d9e1a78e08ae938d3a40
Roman <safonov.roman.86@yandex.ru>
2022-07-27 14:06:55 +1000

    Second try

--- src/main/java/ru/safonovroman/kata/crud/config/AppInit.java
+++ src/main/java/ru/safonovroman/kata/crud/config/AppInit.java
@@ -1,21 +1,8 @@
 package ru.safonovroman.kata.crud.config;
 
-import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-import ru.safonovroman.kata.crud.filter.CharacterSetFilter;
-
-import javax.servlet.Filter;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
-
-    @Override
-    protected Filter[] getServletFilters() {
-        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
-        encodingFilter.setForceEncoding(true);
-        encodingFilter.setEncoding("UTF-8");
-
-        return new Filter[]{encodingFilter};
-    }
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[]{DatabaseConfig.class};
